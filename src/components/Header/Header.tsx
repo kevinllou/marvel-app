5a541b7fb156ab8e43c30021bc7e8107819f3ee0
kevinllo <kevingrande2002@gmail.com>
2023-04-04 11:34:19 -0600

    ADD: Header component and routes constants

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from 'react';
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
+import MarvelIcon from '../../assets/marvel-icon.png';
+import ROUTES from '../../routes/routes';
+
+function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <section className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          <img src={MarvelIcon} alt="marvel logo" className="header__bgImage" />
+        </div>
+        <nav className={isOpen ? 'header__navigation--active' : 'header__navigation'}>
+          <ul className="header__navList">
+            <li className="header__navItems">
+              <NavLink
+                to={ROUTES.HOME}
+                className={({ isActive }) => (isActive ? 'header__activeLink' : '')}
+              >
+                Home
+              </NavLink>
+
+            </li>
+            <li className="header__navItems">
+              <NavLink
+                to={ROUTES.CHARACTERS}
+                className={({ isActive }) => (isActive ? 'header__activeLink' : '')}
+              >
+                Characters
+              </NavLink>
+
+            </li>
+            <li className="header__navItems">
+              <NavLink
+                to={ROUTES.COMICS}
+                className={({ isActive }) => (isActive ? 'header__activeLink' : '')}
+              >
+                Comics
+              </NavLink>
+
+            </li>
+            <li className="header__navItems">
+              <NavLink
+                to={ROUTES.STORIES}
+                className={({ isActive }) => (isActive ? 'header__activeLink' : '')}
+              >
+                Stories
+              </NavLink>
+
+            </li>
+            <li className="header__navItems">
+              <NavLink
+                to={ROUTES.BOOKMARKS}
+                className={({ isActive }) => (isActive ? 'header__activeLink' : '')}
+              >
+                Bookmarks
+              </NavLink>
+
+            </li>
+          </ul>
+        </nav>
+        <div className="header__menuIcon" onClick={toggleMenu} role="menu" tabIndex={0}>Menu</div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/routes/routes.ts
@@ -0,0 +1,12 @@
+const ROUTES = {
+  HOME: '/',
+  CHARACTERS: '/characters',
+  CHARACTERS_ID: '/characters/:id',
+  COMICS: '/comics',
+  COMICS_ID: '/comics/:id',
+  STORIES: '/stories',
+  STORIES_ID: '/stories/:id',
+  BOOKMARKS: '/bookmarks',
+};
+
+export default ROUTES;
