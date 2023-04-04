117851228f033129fd6167169c881ffe92444ec9
kevinllo <kevingrande2002@gmail.com>
2023-04-04 11:50:43 -0600

    ADD: Hero and News added to Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Hero from '../../components/Home/Hero';
+import News from '../../components/News/News';
+import './Home.scss';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <News />
+    </>
+  );
+}
+
+export default Home;
