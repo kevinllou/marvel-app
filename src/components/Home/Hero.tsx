c10499bcb2bf69812d4d8adf1325e9fa9a6035fe
kevinllo <kevingrande2002@gmail.com>
2023-04-04 11:36:12 -0600

    ADD: Hero section

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+function Hero() {
+  return (
+
+    <section className="hero">
+      <div className="hero__title">
+        <h1>THIS WEEK'S NEW COMICS</h1>
+      </div>
+      <div className="hero__description">
+        <p>
+          Take a swim with the Jeff the Land Shark, face the Beyonder with the Avengers, witness
+          the final battle for the soul of Marvel magic, and more in this week's comics!
+        </p>
+      </div>
+      <div className="hero__button">
+        <button type="button">Learn more</button>
+      </div>
+    </section>
+
+  );
+}
+
+export default Hero;
